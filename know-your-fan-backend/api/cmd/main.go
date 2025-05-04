603756da531dbfefd667c6f451698a9f2be5b4c3
Kadu <kaduzera.ks@gmail.com>
2025-05-04 05:50:19 -0300

    feat: setup web server

---
+++ know-your-fan-backend/api/cmd/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	"github.com/KaduSantanaDev/know-your-fan-api/adapters/database"
+	handlers "github.com/KaduSantanaDev/know-your-fan-api/adapters/http"
+	"github.com/KaduSantanaDev/know-your-fan-api/application/service"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	_ "modernc.org/sqlite"
+)
+
+func main() {
+	db, err := sql.Open("sqlite", "./cmd/data.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	clientDB := database.NewClientDB(db)
+	clientService := service.NewClientService(*clientDB)
+	clientHandler := handlers.NewClientHandler(*clientService)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Post("/api/v1/clients", clientHandler.Create)
+
+	log.Println("Servidor ouvindo em http://localhost:3031")
+	if err := http.ListenAndServe(":3031", r); err != nil {
+		log.Fatal(err)
+	}
+}
