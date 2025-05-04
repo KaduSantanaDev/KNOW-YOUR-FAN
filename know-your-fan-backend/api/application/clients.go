c486f3f3f6a199aaab0906b022738cf733bc70bd
Kadu <kaduzera.ks@gmail.com>
2025-05-04 03:48:42 -0300

    feat: create getters for domain entity

--- know-your-fan-backend/api/application/clients.go
+++ know-your-fan-backend/api/application/clients.go
@@ -1,9 +1,47 @@
 package application
 
+import (
+	"github.com/google/uuid"
+)
+
 type Client struct {
 	ID       string
+	Name     string
 	Email    string
 	CPF      string
 	Document []byte
 	Address  Address
+}
+
+type ClientInterface interface {
+	GetID() string
+	GetEmail() string
+	GetDocument() string
+	GetAddress() Address
+}
+
+type ClientServiceInterface interface {
+	Create(name, email, cpf string, address Address)
+}
+
+func (c *Client) GetID() string {
+	return c.ID
+}
+func (c *Client) GetName() string {
+	return c.Name
+}
+func (c *Client) GetEmail() string {
+	return c.Email
+}
+func (c *Client) GetCPF() string {
+	return c.CPF
+}
+func (c *Client) GetAddress() Address {
+	return c.Address
+}
+
+func NewClient() *Client {
+	return &Client{
+		ID: uuid.New().String(),
+	}
 }
--- know-your-fan-backend/api/go.mod
+++ know-your-fan-backend/api/go.mod
@@ -1,3 +1,5 @@
 module github.com/KaduSantanaDev/know-your-fan-api
 
 go 1.23.8
+
+require github.com/google/uuid v1.6.0
