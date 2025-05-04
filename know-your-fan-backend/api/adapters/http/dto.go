7aaaaa35bc8a427cda0661f32515f39c81ed069c
Kadu <kaduzera.ks@gmail.com>
2025-05-04 05:49:29 -0300

    feat: create CreateClientDTO

---
+++ know-your-fan-backend/api/adapters/http/dto.go
@@ -0,0 +1,15 @@
+package handlers
+
+type CreateClientDTO struct {
+	Name         string `json:"name"`
+	Email        string `json:"email"`
+	CPF          string `json:"cpf"`
+	Document     []byte `json:"document"`
+	Street       string `json:"street"`
+	Number       int32  `json:"number"`
+	Complement   string `json:"complement"`
+	Neighborhood string `json:"neighborhood"`
+	City         string `json:"city"`
+	State        string `json:"state"`
+	CEP          string `json:"cep"`
+}
