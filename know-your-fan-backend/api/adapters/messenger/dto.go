ff0dd581d3274e8d80d06344c0fce1d29a1b9cec
KaduSantanaDev <kaduzera.ks@gmail.com>
2025-05-04 22:38:22 -0300

    create a message reciever dto

--- know-your-fan-backend/api/adapters/messenger/dto.go
+++ know-your-fan-backend/api/adapters/messenger/dto.go
@@ -6,3 +6,9 @@ type ClientCreatedEvent struct {
 	Document []byte `json:"document"`
 	Valid    *bool  `json:"valid"`
 }
+
+type RecieveClientEvent struct {
+	ID    string `json:"id"`
+	Name  string `json:"name"`
+	Valid *bool  `json:"valid"`
+}
